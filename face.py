3feea8c2e611d97b9118bd54672c19e7e2d44b59
DavidSantosTXUSA <dsantos4148@gmail.com>
2021-10-02 18:28:45 -0400

    Update face.py

--- face.py
+++ face.py
@@ -1,8 +1,30 @@
 import cv2
+import os
+
+cascPath=os.path.dirname(cv2.__file__)+"/data/haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
 
 video = cv2.VideoCapture(0)
 
 while True:
-    frame = video.read()
+    ret, frame = video.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30,30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
 
     cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
