dd668de449d301e65dbbaf014710360d61a006b4
DavidSantosTXUSA <dsantos4148@gmail.com>
2021-10-02 19:33:18 -0400

    DA

--- Website/script.js
+++
@@ -1,6 +0,0 @@
-function getRandomLetter() {
-                            var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-                            var result = '';
-                            const randomChar = alphabet[Math.floor(Math.random() * alphabet.length)]
-                            return randomChar;
-                        }
